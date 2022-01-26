d7b9103e702143c0886fed6df91df20fed0fa456
RajSolai <msraj085@gmail.com>
2022-01-26 19:53:39 +0530

    added selflink in brand

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 
 type Props = {};
+const domain = location.href;
 
 export default function Brand({}: Props) {
   return (
     <>
-      <div className="flex items-center">
-        <img
-          src="logo.jpg"
-          style={{ height: "60px" }}
-          className="mr-2 rounded-md shadow-sm"
-          alt=""
-        />
-        <h2 className="brand-text">
-          Trading Squad
-        </h2>
-      </div>
+      <a href={domain}>
+        <div className="flex items-center">
+          <img
+            src="logo.jpg"
+            style={{ height: "60px" }}
+            className="mr-2 rounded-md shadow-sm"
+            alt=""
+          />
+          <h2 className="brand-text">Trading Squad</h2>
+        </div>
+      </a>
     </>
   );
 }
